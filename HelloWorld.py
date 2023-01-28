af020a644c9bfdf9dfd4c053356448ce4d9833c0
ankitamondal9 <118752901+ankitamondal9@users.noreply.github.com>
2023-01-29 00:56:12 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,19 @@
 print("Hello World!")
+
+print ("Press 1 for reverse\n Press 2 for sum of the product")
+
+ch = int(input())
+print ("Enter the first input -")
+a = int(input())
+print ("Enter the second input -")
+b = int(input())
+
+match ch:
+    case 1: num = 1542
+      sum = 0
+      while num != 0
+      dig = num % 10
+      sum = sum*10+ dig
+      num = math.floor(num/10)
+      print (sum)
+    case _: print("Wrong choice")
